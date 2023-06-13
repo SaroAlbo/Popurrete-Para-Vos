3d93c8709ebc0a339c76d87278edee3a7403fe5f
SaroAlbo <saromanuelalbo@hotmail.com>
2023-06-13 15:46:58 -0300

    cambios

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let edad = prompt("Ingrese edad")
+
+if( edad < 17) {
+    alert ("Usted es mayor de edad y puede acceder a todo el menu")
+} else {
+    alert ("Usted es menor de edad y no puede acceder al menu")
+}
